0e4b81fa0ca863f832883603232c2745307cb8df
Thiep Tran Trung (SDC6.SDC6.BU3) <trantrungthiep13022000@gmail.com>
2026-03-18 15:02:19 +0700

    update data magic

--- library-managerment-service/src/main/java/com/example/librarymanagermentservice/common/Constant.java
+++ library-managerment-service/src/main/java/com/example/librarymanagermentservice/common/Constant.java
@@ -8,4 +8,6 @@ public final class Constant {
 
     public static final Integer MIN_SIZE = 1;
     public static final Integer MAX_SIZE = 10;
+    public static final Integer DEFAULT_PAGE = 0;
+    public static final String DEFAULT_SOFT_BY = "createdAt";
 }
--- library-managerment-service/src/main/java/com/example/librarymanagermentservice/dto/request/BookSearchRequestDTO.java
+++ library-managerment-service/src/main/java/com/example/librarymanagermentservice/dto/request/BookSearchRequestDTO.java
@@ -1,5 +1,6 @@
 package com.example.librarymanagermentservice.dto.request;
 
+import com.example.librarymanagermentservice.common.Constant;
 import com.example.librarymanagermentservice.common.enums.SortDirectionEnum;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,12 +13,11 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class BookSearchRequestDTO {
-
     private String searchTerm;
     private Long genreId;
     private Boolean availableOnly;
-    private Integer page = 0;
-    private Integer size = 10;
-    private String sortBy = "createdAt";
+    private Integer page = Constant.DEFAULT_PAGE;
+    private Integer size = Constant.MAX_SIZE;
+    private String sortBy = Constant.DEFAULT_SOFT_BY;
     private String sortDirection = SortDirectionEnum.DESC.toString();
 }
